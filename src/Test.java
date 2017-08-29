9247f43f5aa53444aaa6e28292f9412dadd7a2fa
kgullanat <30823949+kgullanat@users.noreply.github.com>
2017-08-29 14:36:54 +0700

    add salary

--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Manager m1 = new Manager();
-		m1.setB(200000);
+		m1.setB(65000);
 		Secretary s1 = new Secretary();
-		s1.setB(200000);
+		s1.setB(65000);
 		Programmer p1 = new Programmer();
-		p1.setB(200000);
+		p1.setB(65000);
 
 	}
 
